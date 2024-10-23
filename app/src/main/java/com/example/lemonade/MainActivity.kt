0af716d0139dba6949a40bcc673807f3b7d3570c
TheShamRio <xsham@inbox.ru>
2024-10-23 17:14:28 +0300

    Реализована основная логика игры с изменением шагов.

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -49,6 +50,32 @@ fun LemonadeApp() {
                 }
             )
         }
-    ) {
+    ) { innerPadding ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            when (currentStep) {
+                1 -> {
+                    // Переход к сжатию лимона
+                    currentStep = 2
+                    squeezeCount = (2..4).random()
+                }
+                2 -> {
+                    // Сокращение числа сжатий
+                    squeezeCount--
+                    if (squeezeCount == 0) currentStep = 3
+                }
+                3 -> {
+                    // Переход к питью лимонада
+                    currentStep = 4
+                }
+                4 -> {
+                    // Начало заново
+                    currentStep = 1
+                }
+            }
+        }
     }
 }
